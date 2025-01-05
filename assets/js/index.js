4ad94d7c3b79ddac25208507e88492646d4de0fb
Libasse Yade <lyade@gainde2000.sn>
2025-01-05 14:43:50 +0000

    senico

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
-const habungeBuntton = document.querySelector('.nav-toggler')
-const navigation = document.querySelector('nav')
- 
-habungeBuntton.addEventListener('click', toggleNav) 
+const navToggler = document.querySelector('.nav-toggler');
+const navigation = document.querySelector('nav');
+
+navToggler.addEventListener('click', toggleNav);
 
 function toggleNav() {
-  habungeBuntton.classList.toggle('active')
-  navigation.classList.toggle('active')
+    navToggler.classList.toggle('active');
+    navigation.classList.toggle('active');
 }
 
 
